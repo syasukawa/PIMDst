9f77705e4ea6994a0bb7897aa6b82eb346732fe5
Shinsuke Yasukawa <shinsuke.yasukawa@gmail.com>
2019-05-14 11:05:41 +0900

    delete files

--- 01/avefunc.m
+++
@@ -1,9 +0,0 @@
-%% ------------------------------------
-% make script files
-%% ------------------------------------
-function y = avefunc(x)
-if ~isvector(x)
-    error('Input must be a vector')
-end
-y = sum(x)/length(x); 
-end
--- 01/ifthen.m
+++
@@ -1,5 +0,0 @@
-x = rand;
-if x > 0.5
-	y = 3; 
-    %Executed only if x > 0.5 
-end
